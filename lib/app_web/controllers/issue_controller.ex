cf3c7482000f5fe7644b435e82a3c002da9a9a60
Cleop <cleo.pearson@hotmail.co.uk>
2018-03-05 17:02:02 +0000

    #47 Displays versions of the comment in order

--- lib/app_web/controllers/issue_controller.ex
+++ lib/app_web/controllers/issue_controller.ex
@@ -2,13 +2,33 @@ defmodule AppWeb.IssueController do
   use AppWeb, :controller
   alias AppWeb.AWS.S3
   alias Poison
-  alias App.{Issue, Repo}
+  alias App.{Issue, Repo, Version}
 
   def show(conn, params) do
     {:ok, %{body: comments}} = S3.get_issue(params["id"])
-    data = Poison.decode!(comments)
-    issue = Repo.get_by!(Issue, issue_id: params["id"])
-    # preload comments and versions
-    render conn, "index.html", comments: data, issue_title: issue.title, comment_inserted: NaiveDateTime.truncate(issue.inserted_at, :second)
+    data =
+      comments
+      |> Poison.decode!
+      |> Map.to_list
+
+    issue =
+      Repo.get_by!(Issue, issue_id: params["id"])
+      |> Repo.preload([comments: [:versions]])
+
+    comment_versions =
+      issue.comments
+      |> Enum.at(0)
+      |> Map.get(:versions)
+      |> Enum.map(fn version -> version.inserted_at end)
+      |> Enum.map(fn version_inserted -> NaiveDateTime.truncate(version_inserted, :second) end)
+
+    combined_data = List.zip([data, comment_versions]) |> Enum.reverse
+
+    [latest_version | rest] = combined_data
+    {{lv_id, lv_content}, lv_inserted} = latest_version
+
+    render(conn, "index.html", latest_version_content: lv_content,
+    latest_version_inserted: lv_inserted, latest_version_id: lv_id, rest: rest,
+    issue_title: issue.title)
   end
 end
