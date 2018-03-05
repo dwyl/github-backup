4d6fa4390a1ea24eb074283befc9691747418feb
Cleop <cleo.pearson@hotmail.co.uk>
2018-03-05 11:51:51 +0000

    #40 Displays date/issue title on frontend

--- lib/app_web/controllers/issue_controller.ex
+++ lib/app_web/controllers/issue_controller.ex
@@ -5,16 +5,10 @@ defmodule AppWeb.IssueController do
   alias App.{Issue, Repo}
 
   def show(conn, params) do
-    IO.inspect params # {"id" => "1"}
-    # get the file with is params["id"].json
     {:ok, %{body: comments}} = S3.get_issue(params["id"])
     data = Poison.decode!(comments)
-    #  Get the data from the db
-    IO.inspect String.to_integer(params["id"])
     issue = Repo.get_by!(Issue, issue_id: params["id"])
     # preload comments and versions
-    IO.inspect issue
-    IO.inspect "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    render conn, "index.html", comments: data
+    render conn, "index.html", comments: data, issue_title: issue.title, comment_inserted: NaiveDateTime.truncate(issue.inserted_at, :second)
   end
 end
