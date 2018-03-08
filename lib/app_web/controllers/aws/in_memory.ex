049f5d6e3d5f283f894136d17f4c03d369002d54
simonLab <simon.labondance@gmail.com>
2018-03-08 17:09:49 +0000

    build dynamic s3 mock, #53

--- lib/app_web/controllers/aws/in_memory.ex
+++ lib/app_web/controllers/aws/in_memory.ex
@@ -2,13 +2,25 @@ defmodule AppWeb.AWS.InMemory do
   @moduledoc """
   mock of S3 api functions for tests
   """
+  alias App.{Issue, Repo}
 
   def save_comment(_issue_id, _comment) do
     %{ok: %{}}
   end
 
-  def get_issue(_issue_id) do
-    {:ok, %{body: "{\"1\": \"comment\"}"}}
+  def get_issue(issue_id) do
+    issue = Repo.get_by!(Issue, issue_id: issue_id)
+    issue_details = issue
+      |> Repo.preload([comments: [:versions]])
+
+    versions = Enum.flat_map(issue_details.comments, fn c ->
+                c.versions
+              end)
+    json = Enum.reduce(versions, %{}, fn(v, acc) ->
+      Map.put(acc, "#{v.id}", "comment")
+    end)
+
+    {:ok, %{body: Poison.encode!(json)}}
   end
 
 end
