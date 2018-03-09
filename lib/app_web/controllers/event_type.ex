4a16b8ea41646159ad60ee08e920b73bd125a2bf
Cleop <cleo.pearson@hotmail.co.uk>
2018-03-09 11:03:14 +0000

    #66 Comments out code not in use/not being tested

--- lib/app_web/controllers/event_type.ex
+++ lib/app_web/controllers/event_type.ex
@@ -8,7 +8,9 @@ defmodule AppWeb.EventType do
   def get_event_type(x_github_event, action, conn, payload) do
     case x_github_event do
       "installation" -> type("installation", action, conn, payload)
-      "installation_repositories" -> type("installation_repositories", action, conn, payload)
+      # See #72 - https://git.io/vApsZ
+      # "installation_repositories" ->
+         # type("installation_repositories", action, conn, payload)
       "issues" -> type("issues", action, conn, payload)
       "issue_comment" -> type("issue_comment", action, conn, payload)
       _ -> nil
@@ -21,18 +23,21 @@ defmodule AppWeb.EventType do
     end
   end
 
-  defp type("installation_repositories", action) do
-    case action do
-      "added" -> :new_installation_repositories
-    end
-  end
+  # See #72 - https://git.io/vApsZ
+  # defp type("installation_repositories", action) do
+  #   case action do
+  #     "added" -> :new_installation_repositories
+  #   end
+  # end
 
   defp type("issues", action, conn, payload) do
     case action do
       "opened" -> EventTypeHandlers.issue_created(conn, payload)
       "edited" -> EventTypeHandlers.issue_edited(conn, payload)
-      "closed" -> :issue_closed
-      "reopened" -> :issue_reopened
+      # See #70 - https://git.io/vAp3y
+      # "closed" -> :issue_closed
+      # See #71 - https://git.io/vAp3b
+      # "reopened" -> :issue_reopened
     end
   end
 
