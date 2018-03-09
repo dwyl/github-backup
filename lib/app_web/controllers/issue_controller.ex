75e4fbb6c0f4280970af1fefe6566b33a4475c4e
Cleop <cleo.pearson@hotmail.co.uk>
2018-03-09 18:06:48 +0000

    #75 Gets issue text from s3

--- lib/app_web/controllers/issue_controller.ex
+++ lib/app_web/controllers/issue_controller.ex
@@ -1,7 +1,29 @@
 defmodule AppWeb.IssueController do
   use AppWeb, :controller
+  alias Poison
+  alias App.{Comment, Repo, Version}
+  import Ecto.Query
 
-  def show(conn, _params) do
-    render(conn, "index.html")
+  @s3_api Application.get_env(:app, :s3_api)
+
+  def show(conn, params) do
+    issue_id = params["id"]
+
+
+    # Get values from S3
+    {:ok, %{body: comments_text}} = @s3_api.get_issue(issue_id)
+
+    comments_text =
+      comments_text
+      |> Poison.decode!
+
+    IO.inspect(comments_text)
+
+    render(
+      conn,
+      "index.html"
+      comments_text: comments_text,
+    )
+
   end
 end
