9fec08ab3e86184923b35d050b02f10fbb9792cf
Cleop <cleo.pearson@hotmail.co.uk>
2018-03-15 18:22:11 +0000

    #70 Displays Issue Status on the frontend

--- test/app_web/controllers/issue_controller_test.exs
+++ test/app_web/controllers/issue_controller_test.exs
@@ -14,10 +14,34 @@ defmodule AppWeb.IssueControllerTest do
           }
         ]
       }
+      issue_params2 = %{
+              issue_id: 2,
+              title: "Test issue title2",
+              comments: [
+                %{
+                  comment_id: "1_2",
+                  versions: [%{author: "SimonLab"}]
+                }
+              ],
+              issue_status: [
+                %{
+                  event: "closed"
+                }
+              ]
+            }
 
       changeset = Issue.changeset(%Issue{}, issue_params)
       issue = Repo.insert!(changeset)
-      {:ok, conn: build_conn() |> assign(:issue, issue)}
+
+      changeset2 = Issue.changeset(%Issue{}, issue_params2)
+      issue2 = Repo.insert!(changeset2)
+
+
+      {:ok, conn: build_conn() |> assign(:issue, issue) |> assign(:issue2, issue2)}
+    end
+    test "GET /issues/2", %{conn: conn} do
+      conn = get conn, "/issues/2"
+      assert html_response(conn, 200) =~ "Test issue title2"
     end
 
     test "GET /issues/1", %{conn: conn} do
