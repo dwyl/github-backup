b12fa25cd8aa1fa8935b8fa47de86f5d7b69be76
Cleop <cleo.pearson@hotmail.co.uk>
2018-03-19 15:32:09 +0000

    #14 Adds comment to code

--- lib/app_web/controllers/meta_table.ex
+++ lib/app_web/controllers/meta_table.ex
@@ -1,9 +1,11 @@
 defmodule AppWeb.MetaTable do
+alias AppWeb.Endpoint
 
   def get_meta_table(issue_id) do
-    count = AppWeb.Endpoint.url <> "/edit-count/#{issue_id}"
-    history = AppWeb.Endpoint.url <> "/issues/#{issue_id}"
+    count = Endpoint.url <> "/edit-count/#{issue_id}"
+    history = Endpoint.url <> "/issues/#{issue_id}"
     """
+    <!-- do not edit below this line to preserve issue history -->
     | Edits | View issue history |
     |-|-|
     |[![Edit Count](#{count})](#{count}) | **View History:** [#{history}](#{history})|
