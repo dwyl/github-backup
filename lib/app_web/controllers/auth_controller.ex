a8e54c9a10aa47aa9282285d84289ed082469796
robstallion <robertefrancis18@gmail.com>
2018-04-06 18:21:55 +0100

    update auth controller to put the gh user_id in the session

--- lib/app_web/controllers/auth_controller.ex
+++ lib/app_web/controllers/auth_controller.ex
@@ -2,12 +2,12 @@ defmodule AppWeb.AuthController do
   use AppWeb, :controller
   plug Ueberauth
 
-  def callback(conn, _params) do
-    IO.inspect(conn.assigns, label: "----------------> in callback")
-    redirect(conn, to: auth_path(conn, :index))
-  end
+  def callback(conn, params) do
+    gh_user_id = conn.assigns.ueberauth_auth.extra.raw_info.user["id"]
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_session(:user_id, gh_user_id)
+    |> redirect(to: issue_path(conn, :show, 236297481)) #update hardcoded issue
+    # |> configure_session(renew: true)
   end
 end
